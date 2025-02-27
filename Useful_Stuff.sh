79bc89a798870caf7e35f97f1248438c697494e3
Lars Littmann <larslitt@login1.head.dardel.pdc.kth.se>
2025-02-27 09:37:22 +0100

    Fix typos and add super useful code

--- Useful_Stuff.sh
+++ Useful_Stuff.sh
@@ -4,4 +4,12 @@
 
 echo "Wow, this sure is some useful code!"
 
-# woah ehre comessome nice nbionfmatics and i have rsi much code
+# woah here comes some nice bioinformatics and i have so much code
+
+ANIMAL="yes"
+
+if (${ANIMAL} == "yes"); do
+echo "yep, that's an animal";
+else;
+echo "nah yeah nah that's not an animal";
+done
